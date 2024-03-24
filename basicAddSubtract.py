21de65a4874a9c468afa1f76bc8f81dd93a04e17
cb1002 <164748203+cb1002@users.noreply.github.com>
2024-03-24 07:13:38 -0500

    Add files via upload

---
+++ basicAddSubtract.py
@@ -0,0 +1,18 @@
+# input for two numbers
+num1 = float(input("Enter first number (num1): "))
+num2 = float(input("Enter second number (num2): "))
+
+# Add 
+addition = num1 + num2
+
+# Subtract 
+subtraction = num1 - num2
+
+# Show the answers
+print(f"The addition of input {num1} and {num2} is: {addition}")
+print(f"The subtraction of input {num1} from {num2} is: {subtraction}")
+
+
+
+
+
---
+++ basicMultiplyDivide.py
@@ -0,0 +1,14 @@
+# input for two numbers
+num1 = float(input("Enter first number (num1): "))
+num2 = float(input("Enter second number (num2): "))
+
+# Multiply 
+multiplication = num1 * num2
+
+# Divide 
+division = num1 / num2
+
+# Show the answers
+print(f"The multiplication of input {num1} and {num2} is: {multiplication}")
+print(f"The division of input {num1} and {num2} is: {division}")
+
